4020eb8836c7e333c5ba43fcd70fd2cb51f9a25f
mo-hunter123 <ahrrass.00@gmail.com>
2021-04-01 14:59:04 +0100

    Add structures

--- todo.c
+++ todo.c
@@ -1,10 +1,7 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
 #include <string.h>
-// typedef struct todoArea{
-//     GtkWidget *entry;
-//     GtkWidget *button;
-// }todoArea
+
 
 //structur pour une tache a realise 
 typedef struct {
@@ -13,50 +10,55 @@ typedef struct {
     GtkWidget *buttonDeletion;//supprinner la todo 
 }todoItem;
 
-GtkWidget *entry;
 GtkWidget *vbox;
+
+typedef struct {
+    GtkWidget *entry;
+    GtkWidget *addButton;
+    GtkWidget *label;
+}EntryArea;
+
 
 static void deleteTodo(GtkWidget *button, todoItem *data)
 {
-    
-
     gtk_widget_destroy(data->todoLabel);
     gtk_widget_destroy(data->buttonDone);
     gtk_widget_destroy(data->buttonDeletion);
-
-
 }
 
 static void doneTodo(GtkWidget *button, todoItem *data)
 {
-    const char *format = "<span size=\"x-large\" strikethrough=\"true\"><b>%s</b></span>";
+    const char *format = "<span size=\"x-large\"><s><b>%s</b></s></span>";
     char *markup;
     const char *buff = gtk_label_get_text(GTK_LABEL(data->todoLabel));
     printf("\n%s\n", buff);
     markup = g_markup_printf_escaped (format, buff);
-    // gtk_label_set_label(GTK_LABEL(), buff);
     gtk_label_set_markup (GTK_LABEL (data->todoLabel), markup);
     g_free (markup);
 }
 
     
 
-static void addTodo(GtkWidget* button, gpointer data)
+static void addTodo(GtkWidget* button, EntryArea *data)
 {
     char *buff;
-    int size = strlen(gtk_entry_get_text(GTK_ENTRY(entry)));
+    int size = strlen(gtk_entry_get_text(GTK_ENTRY(data->entry)));
     buff = (char*)malloc(size);
-    strcpy(buff, gtk_entry_get_text(GTK_ENTRY(entry)));
+    strcpy(buff, gtk_entry_get_text(GTK_ENTRY(data->entry)));
     
 
     printf("%s\n", buff);
     // label to add 
-    if(size != 0){
+    if(size != 0)
+    {
         GtkWidget *hbox;
         hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 1);
         gtk_container_add(GTK_CONTAINER(vbox), hbox);
+
+        //declaration de la tache a faire 
         todoItem *todo = (todoItem*)malloc(sizeof(todoItem));
         todo->todoLabel = gtk_label_new(buff);
+
         const char *format = "<span foreground=\"#dfe6e9\" size=\"x-large\" font_style=\"normal\"><b>%s</b></span>";
         char *markup;
 
@@ -65,7 +67,6 @@ static void addTodo(GtkWidget* button, gpointer data)
         markup = g_markup_printf_escaped (format, buff);
         gtk_label_set_markup (GTK_LABEL (todo->todoLabel), markup);
         g_free (markup);
-
 
         gtk_container_add(GTK_CONTAINER(hbox), todo->todoLabel);
 
@@ -79,7 +80,7 @@ static void addTodo(GtkWidget* button, gpointer data)
         g_signal_connect(todo->buttonDeletion, "clicked", G_CALLBACK(deleteTodo), (todoItem *) todo);
         g_signal_connect(todo->buttonDone, "clicked", G_CALLBACK(doneTodo), (todoItem *) todo);
         
-        gtk_entry_set_text(GTK_ENTRY(entry), "");
+        gtk_entry_set_text(GTK_ENTRY(data->entry), "");
     }
     
     gtk_widget_show_all(vbox);
@@ -96,9 +97,7 @@ static void activate(GtkApplication *App, gpointer user_data)
     GtkCssProvider *cssProvider = gtk_css_provider_new();
     GtkStyleContext *WindowContext, *EntryContext, *HboxContext, *LabelContext;
 
-
-    GtkWidget *scrollBar;
-
+    EntryArea *entryAdd = (EntryArea*)malloc(sizeof(EntryArea));
 
     vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 1);
     gtk_container_add(GTK_CONTAINER(window), vbox);
@@ -107,21 +106,17 @@ static void activate(GtkApplication *App, gpointer user_data)
     WindowContext = gtk_widget_get_style_context(vbox);
     gtk_style_context_add_class(WindowContext, "M-window");
    
-    GtkWidget *label;
-    
     const char *buff = "AHRRASS' TODO List";
 
-    label = gtk_label_new(buff);
+    entryAdd->label = gtk_label_new(buff);
     const char *format = "<span foreground=\"#dfe6e9\" size=\"x-large\" font_style=\"normal\"><b>%s</b></span>";
     char *markup;
 
-    // pango_parse_markup()
-
     markup = g_markup_printf_escaped (format, buff);
-    gtk_label_set_markup (GTK_LABEL (label), markup);
+    gtk_label_set_markup (GTK_LABEL (entryAdd->label), markup);
     g_free (markup);
     //add label
-    gtk_container_add(GTK_CONTAINER(vbox), label);
+    gtk_container_add(GTK_CONTAINER(vbox), entryAdd->label);
 
     GtkWidget* hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 1);
     gtk_container_add(GTK_CONTAINER(vbox), hbox);
@@ -129,18 +124,18 @@ static void activate(GtkApplication *App, gpointer user_data)
     HboxContext = gtk_widget_get_style_context(vbox);
     gtk_style_context_add_class(HboxContext, "M-hbox");
 
-    entry = gtk_entry_new();
-    gtk_editable_set_editable(GTK_EDITABLE(entry), TRUE);
-    gtk_container_add(GTK_CONTAINER(hbox), entry);
+    entryAdd->entry = gtk_entry_new();
+    gtk_editable_set_editable(GTK_EDITABLE(entryAdd->entry), TRUE);
+    gtk_container_add(GTK_CONTAINER(hbox), entryAdd->entry);
 
     EntryContext = gtk_widget_get_style_context(vbox);
     gtk_style_context_add_class(EntryContext, "M-entry");
 
-    button = gtk_button_new_from_icon_name(GTK_STOCK_ADD, GTK_ICON_SIZE_BUTTON);
-    gtk_container_add(GTK_CONTAINER(hbox), button);
+    entryAdd->addButton = gtk_button_new_from_icon_name(GTK_STOCK_ADD, GTK_ICON_SIZE_BUTTON);
+    gtk_container_add(GTK_CONTAINER(hbox), entryAdd->addButton);
     
 
-    g_signal_connect(button, "clicked", G_CALLBACK(addTodo), NULL);
+    g_signal_connect(entryAdd->addButton, "clicked", G_CALLBACK(addTodo), (EntryArea *) entryAdd);
      
     /*Css*/
     gtk_css_provider_load_from_path(cssProvider, "styles.css", NULL);
